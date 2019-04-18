3ea3b3aa7a719b46a9c54c6e7f8a501da24e8807
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-18 02:53:21 -0500

    Add bootstrap file.

---
+++ bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser PasswordLock - Secure your password using password lock.
+ * 
+ * @package Kooser\PasswordLock.
+ */
+
+/**
+ * Determine if the hash is pbkdf2.
+ *
+ * @param string $hash The hash to check.
+ *
+ * @return bool Returns true if the hash is pbkdf2 and false if not.
+ */
+function is_pbkdf2(string $hash): bool
+{
+    $component = substr($hash, 0, 8);
+    return (bool) ($component == '$pbkdf2$');
+}
