f5dd38ac63b701d7318395a77340ba771f6a7eaa
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-18 03:12:41 -0500

    Test mix functionality.

---
+++ tests/MixTest.php
@@ -0,0 +1,89 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser PasswordLock - Secure your password using password lock.
+ * 
+ * @package Kooser\PasswordLock.
+ */
+
+namespace PasswordLock\Test;
+
+use Kooser\PasswordLock\Argon2i;
+use Kooser\PasswordLock\Argon2id;
+use Kooser\PasswordLock\Bcrypt;
+use Kooser\PasswordLock\Exception\PasswordNeedsRehash\Exception;
+use Kooser\PasswordLock\Pbkdf2;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test mix functionality.
+ */
+class MixTest extends TestCase
+{
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testMix(): void
+    {
+        $pbkdf2 = new Pbkdf2([
+            'algo' => 'sha512',
+            'salt' => 'Kooser'
+        ]);
+        $bcrypt = new Bcrypt([
+            'cost' => 10,
+        ], \false);
+        $argon2i = new Argon2i([], \false);
+        $argon2id = new Argon2id([], \false);
+        $hash = $pbkdf2->compute('KooserPHP!');
+        $this->assertTrue($bcrypt->needsRehash($hash));
+        $this->assertTrue($argon2i->needsRehash($hash));
+        $this->assertTrue($argon2id->needsRehash($hash));
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testMixArgon2i(): void
+    {
+        $this->expectException(PasswordNeedsRehashException::class);
+        $pbkdf2 = new Pbkdf2([
+            'algo' => 'sha512',
+            'salt' => 'Kooser'
+        ]);
+        $argon2i = new Argon2i();
+        $hash = $pbkdf2->compute('KooserPHP!');
+        $argon2i->needsRehash($hash);
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testMixArgon2id(): void
+    {
+        $this->expectException(PasswordNeedsRehash::class);
+        $pbkdf2 = new Pbkdf2([
+            'algo' => 'sha512',
+            'salt' => 'Kooser'
+        ]);
+        $argon2id = new Argon2id();
+        $hash = $pbkdf2->compute('KooserPHP!');
+        $argon2id->needsRehash($hash);
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testMixBcrypt(): void
+    {
+        $this->expectException(PasswordNeedsRehash::class);
+        $pbkdf2 = new Pbkdf2([
+            'algo' => 'sha512',
+            'salt' => 'Kooser'
+        ]);
+        $bcrypt = new Bcrypt(['cost' => 10]);
+        $hash = $pbkdf2->compute('KooserPHP!');
+        $bcrypt->needsRehash($hash);
+    }
+}
