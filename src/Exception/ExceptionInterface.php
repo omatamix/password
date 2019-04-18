2efd7a8b73e2b07be1b5c16d8fe7b022d36ad425
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-18 01:26:40 -0500

    Add exception interface.

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser PasswordLock - Secure your password using password lock.
+ * 
+ * @package Kooser\PasswordLock.
+ */
+
+namespace Kooser\PasswordLock\Exception;
+
+use Throwable;
+
+/**
+ * The interface for all exceptions.
+ */
+interface ExceptionInterface extends Throwable
+{
+}
