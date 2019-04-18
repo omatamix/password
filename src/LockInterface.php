f838c46ee1c16874da9dc79d705855c8fd63bfb5
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-18 02:04:37 -0500

    Add the lock interface.

---
+++ src/LockInterface.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser PasswordLock - Secure your password using password lock.
+ * 
+ * @package Kooser\PasswordLock.
+ */
+
+namespace Kooser\PasswordLock;
+
+/**
+ * The hasher interface.
+ */
+interface LockInterface
+{
+}
