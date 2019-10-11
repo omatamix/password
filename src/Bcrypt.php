d8125b05fb654e9e47e39fef6a0ec9232c203189
Nicholas <48822466+Kooser6@users.noreply.github.com>
2019-10-11 07:02:31 -0400

    Update Bcrypt.php

--- src/Bcrypt.php
+++ src/Bcrypt.php
@@ -41,7 +41,7 @@ final class Bcrypt extends AbstractLock implements LockInterface
      *
      * @param array $options The bcrypt hasher options.
      *
-     * @return \Kooser\PasswordLock\LockInterface Returns the hasher.
+     * @return \Kooser\Framework\PasswordLock\LockInterface Returns the hasher.
      */
     public function setOptions(array $options = []): LockInterface
     {
@@ -56,7 +56,7 @@ final class Bcrypt extends AbstractLock implements LockInterface
      *
      * @param bool $exceptions Should we utilize exceptions.
      *
-     * @return \Kooser\PasswordLock\LockInterface Returns the hasher.
+     * @return \Kooser\Framework\PasswordLock\LockInterface Returns the hasher.
      */
     public function setExceptions(bool $exceptions = \true): LockInterface
     {
