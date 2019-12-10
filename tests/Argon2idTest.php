e1632aa52a7d80cd76ac0ab48694879f55226afb
omatamix <47761650+omatamix@users.noreply.github.com>
2019-12-10 00:22:38 -0500

    Update Argon2idTest.php

--- tests/Argon2idTest.php
+++ tests/Argon2idTest.php
@@ -1,16 +1,11 @@
 <?php
 declare(strict_types=1);
-/**
- * Kooser PasswordLock - Secure your password using password lock.
- * 
- * @package Kooser\PasswordLock.
- */
 
 namespace PasswordLock\Test;
 
-use Kooser\Framework\PasswordLock\Argon2id;
-use Kooser\Framework\PasswordLock\Exception\PasswordNeedsRehashException;
-use Kooser\Framework\PasswordLock\Exception\PasswordNotVerifiedException;
+use PasswordLock\Argon2id;
+use PasswordLock\Exception\PasswordNeedsRehashException;
+use PasswordLock\Exception\PasswordNotVerifiedException;
 
 use PHPUnit\Framework\TestCase;
 
