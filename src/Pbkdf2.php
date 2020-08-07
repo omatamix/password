9f7e3617a8975dfb6f28c7fc497a4fc0d54207a3
omatamix <47761650+omatamix@users.noreply.github.com>
2020-08-07 13:33:20 -0400

    Enhance code.

--- src/Pbkdf2.php
+++ src/Pbkdf2.php
@@ -1,31 +1,6 @@
 <?php declare(strict_types=1);
-/**
- * PasswordLock - Secure your password using password lock.
- *
- * @license MIT License. (https://github.com/Commander-Ant-Screwbin-Games/password-lock/blob/master/license)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * https://github.com/Commander-Ant-Screwbin-Games/firecms/tree/master/src/Core
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- * @package Commander-Ant-Screwbin-Games/password-lock.
- */
 
-namespace PasswordLock;
+namespace Omatamix\PasswordLock;
 
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -86,13 +61,13 @@ final class Pbkdf2 extends AbstractLock implements LockInterface
      */
     public function verify(string $password, string $hash): bool
     {
-        if (!$this->exceptions) {
-            return \hash_equals($this->compute($password), $hash);
-        } elseif ($verified = \hash_equals($this->compute($password), $hash)) {
-            return (bool) $verified;
-        } else {
+        
+        if (\hash_equals($this->compute($password), $hash)) {
+            return \true;
+        } elseif ($this->exceptions) {
             throw new Exception\PasswordNotVerifiedException('The password supplied is not verified.');
         }
+        return \false;
     }
 
     /**
@@ -114,9 +89,8 @@ final class Pbkdf2 extends AbstractLock implements LockInterface
             return !(\is_pbkdf2($hash) && $res);
         } elseif ($needsRehash = !(\is_pbkdf2($hash) && $res)) {
             throw new Exception\PasswordNeedsRehashException('The hash supplied needs to be rehashed.');
-        } else {
-            return (bool) $needsRehash;
         }
+        return \false;
     }
 
     /**
