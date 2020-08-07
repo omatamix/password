b15fcfc7d0833fb8325e005da9ae3f06de476875
omatamix <47761650+omatamix@users.noreply.github.com>
2020-08-07 13:17:06 -0400

    Enhance code.

--- src/Bcrypt.php
+++ src/Bcrypt.php
@@ -1,31 +1,6 @@
 <?php declare(strict_types=1);
-/**
- * PasswordLock - Secure your password using password lock.
- *
- * @license MIT License. (https://github.com/Commander-Ant-Screwbin-Games/password-lock/blob/master/license)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * https://github.com/Commander-Ant-Screwbin-Games/firecms/tree/master/src/Core
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- * @package Commander-Ant-Screwbin-Games/password-lock.
- */
 
-namespace PasswordLock;
+namespace Omatamix\PasswordLock;
 
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -35,7 +10,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 final class Bcrypt extends AbstractLock implements LockInterface
 {
 
-    /** @var array $options The bcrypt hasher options. */
+    /** @var array $options The crypt blowfish hasher options. */
     private $options = [];
 
     /** @var bool $exceptions Should we utilize exceptions. */
@@ -74,8 +49,6 @@ final class Bcrypt extends AbstractLock implements LockInterface
      * {@inheritdoc}
      *
      * @throws Exception\PasswordInvalidException If the password supplied is invalid.
-     *
-     * @codeCoverageIgnore
      */
     public function compute(string $password): string
     {
@@ -83,9 +56,8 @@ final class Bcrypt extends AbstractLock implements LockInterface
             \trigger_error('The password supplied is invalid. The max length for a password using bcrypt is 72.', E_USER_ERROR);
         } elseif (\mb_strlen($password) > 72) {
             throw new Exception\PasswordInvalidException('The password supplied is invalid. The max length for a password using bcrypt is 72.');
-        } else {
-            return \password_hash($password, \PASSWORD_BCRYPT, $this->options);
         }
+        return \password_hash($password, \PASSWORD_BCRYPT, $this->options);
     }
 
     /**
@@ -98,11 +70,10 @@ final class Bcrypt extends AbstractLock implements LockInterface
     {
         if (!$this->exceptions) {
             return \password_verify($password, $hash);
-        } elseif ($verified = \password_verify($password, $hash)) {
-            return (bool) $verified;
-        } else {
-            throw new Exception\PasswordNotVerifiedException('The password supplied is not verified.');
+        } elseif (!\password_verify($password, $hash)) {
+            return \true;
         }
+        throw new Exception\PasswordNotVerifiedException('The password supplied is not verified.');
     }
 
     /**
@@ -118,17 +89,11 @@ final class Bcrypt extends AbstractLock implements LockInterface
     public function needsRehash(string $hash): bool
     {
         if (!$this->exceptions) {
-            if ($result = \is_pbkdf2($hash)) {
-                return $result;
-            }
-            return \password_needs_rehash($hash, \PASSWORD_BCRYPT, $this->options);
-        } elseif (\is_pbkdf2($hash)) {
+            return \is_pbkdf2($hash) ? \true : \password_needs_rehash($hash, \PASSWORD_ARGON2ID, $this->options);
+        } elseif (\is_pbkdf2($hash) || \password_needs_rehash($hash, \PASSWORD_ARGON2ID, $this->options)) {
             throw new Exception\PasswordNeedsRehashException('The hash supplied needs to be rehashed.');
-        } elseif ($needsRehash = \password_needs_rehash($hash, \PASSWORD_BCRYPT, $this->options)) {
-            throw new Exception\PasswordNeedsRehashException('The hash supplied needs to be rehashed.');
-        } else {
-            return (bool) $needsRehash;
         }
+        return \false;
     }
 
     /**
