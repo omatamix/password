4b20c6acf343947a6531f695d702d897f83a8a70
Nicholas <47761650+omatamix@users.noreply.github.com>
2021-04-22 03:16:55 -0400

    Cleanup code.

--- src/Bcrypt.php
+++ src/Bcrypt.php
@@ -35,15 +35,11 @@ final class Bcrypt extends AbstractLock implements LockInterface
     /** @var array $options The crypt blowfish hasher options. */
     private $options = [];
 
-    /** @var bool $exceptions Should we utilize exceptions. */
-    protected $exceptions = \true;
-
     /**
      * {@inheritdoc}
      */
-    public function __construct(array $options = [], bool $exceptions = \true)
+    public function __construct(array $options = [])
     {
-        $this->setExceptions($exceptions);
         $this->setOptions($options);
     }
 
@@ -60,42 +56,23 @@ final class Bcrypt extends AbstractLock implements LockInterface
 
     /**
      * {@inheritdoc}
-     */
-    public function setExceptions(bool $exceptions = \true): LockInterface
-    {
-        $this->exceptions = $exceptions;
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
      *
      * @throws Exception\PasswordInvalidException If the password supplied is invalid.
      */
     public function compute(string $password): string
     {
-        if (!$this->exceptions && \mb_strlen($password) > 72) {
-            \trigger_error('The password supplied is invalid. The max length for a password using bcrypt is 72.', E_USER_ERROR);
-        } elseif (\mb_strlen($password) > 72) {
+        if (mb_strlen($password) > 72) {
             throw new Exception\PasswordInvalidException('The password supplied is invalid. The max length for a password using bcrypt is 72.');
         }
-        return \password_hash($password, \PASSWORD_BCRYPT, $this->options);
+        return password_hash($password, PASSWORD_BCRYPT, $this->options);
     }
 
     /**
      * {@inheritdoc}
-     *
-     * @throws Exception\PasswordNotVerifiedException If exceptions enabled throw this exception
-     *                                                if the password is not verified.
      */
     public function verify(string $password, string $hash): bool
     {
-        if (!$this->exceptions) {
-            return \password_verify($password, $hash);
-        } elseif (\password_verify($password, $hash)) {
-            return \true;
-        }
-        throw new Exception\PasswordNotVerifiedException('The password supplied is not verified.');
+        return password_verify($password, $hash);
     }
 
     /**
@@ -103,19 +80,11 @@ final class Bcrypt extends AbstractLock implements LockInterface
      *
      * @param string $hash The hash to check.
      *
-     * @throws Exception\PasswordNeedsRehashException If exceptions enabled throw this exception
-     *                                                if the hash needs a rehash.
-     *
      * @return bool Returns true if the hash needs a rehash and false if not.
      */
     public function needsRehash(string $hash): bool
     {
-        if (!$this->exceptions) {
-            return \is_pbkdf2($hash) ? \true : \password_needs_rehash($hash, \PASSWORD_BCRYPT, $this->options);
-        } elseif (\is_pbkdf2($hash) || \password_needs_rehash($hash, \PASSWORD_BCRYPT, $this->options)) {
-            throw new Exception\PasswordNeedsRehashException('The hash supplied needs to be rehashed.');
-        }
-        return \false;
+        return is_pbkdf2($hash) ? true : password_needs_rehash($hash, PASSWORD_BCRYPT, $this->options);
     }
 
     /**
