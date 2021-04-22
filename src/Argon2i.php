23cdc64cdd7313b310a16a3e51a5ad29193eb7e3
Nicholas <47761650+omatamix@users.noreply.github.com>
2021-04-22 02:55:23 -0400

    Cleanup code.

--- src/Argon2i.php
+++ src/Argon2i.php
@@ -35,15 +35,11 @@ final class Argon2i extends AbstractLock implements LockInterface
     /** @var array $options The argon2i hasher options. */
     private $options = [];
 
-    /** @var bool $exceptions Should we utilize exceptions. */
-    protected $exceptions = \true;
-
     /**
      * {@inheritdoc}
      */
-    public function __construct(array $options = [], bool $exceptions = \true)
+    public function __construct(array $options = [])
     {
-        $this->setExceptions($exceptions);
         $this->setOptions($options);
     }
 
@@ -61,18 +57,9 @@ final class Argon2i extends AbstractLock implements LockInterface
     /**
      * {@inheritdoc}
      */
-    public function setExceptions(bool $exceptions = \true): LockInterface
-    {
-        $this->exceptions = $exceptions;
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function compute(string $password): string
     {
-        return \password_hash($password, \PASSWORD_ARGON2I, $this->options);
+        return password_hash($password, PASSWORD_ARGON2I, $this->options);
     }
 
     /**
@@ -80,12 +67,7 @@ final class Argon2i extends AbstractLock implements LockInterface
      */
     public function verify(string $password, string $hash): bool
     {
-        if (!$this->exceptions) {
-            return \password_verify($password, $hash);
-        } elseif (\password_verify($password, $hash)) {
-            return \true;
-        }
-        throw new Exception\PasswordNotVerifiedException('The password supplied is not verified.');
+        return password_verify($password, $hash);
     }
 
     /**
@@ -93,19 +75,11 @@ final class Argon2i extends AbstractLock implements LockInterface
      *
      * @param string $hash The hash to check.
      *
-     * @throws Exception\PasswordNeedsRehashException If exceptions enabled throw this exception
-     *                                                if the hash needs a rehash.
-     *
      * @return bool Returns true if the hash needs a rehash and false if not.
      */
     public function needsRehash(string $hash): bool
     {
-        if (!$this->exceptions) {
-            return \is_pbkdf2($hash) ? \true : \password_needs_rehash($hash, \PASSWORD_ARGON2I, $this->options);
-        } elseif (\is_pbkdf2($hash) || \password_needs_rehash($hash, \PASSWORD_ARGON2I, $this->options)) {
-            throw new Exception\PasswordNeedsRehashException('The hash supplied needs to be rehashed.');
-        }
-        return \false;
+        return is_pbkdf2($hash) ? true : password_needs_rehash($hash, PASSWORD_ARGON2I, $this->options);
     }
 
     /**
@@ -118,9 +92,9 @@ final class Argon2i extends AbstractLock implements LockInterface
     private function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'memory_cost' => \PASSWORD_ARGON2_DEFAULT_MEMORY_COST,
-            'time_cost'   => \PASSWORD_ARGON2_DEFAULT_TIME_COST,
-            'threads'     => \PASSWORD_ARGON2_DEFAULT_THREADS,
+            'memory_cost' => PASSWORD_ARGON2_DEFAULT_MEMORY_COST,
+            'time_cost'   => PASSWORD_ARGON2_DEFAULT_TIME_COST,
+            'threads'     => PASSWORD_ARGON2_DEFAULT_THREADS,
         ]);
         $resolver->setAllowedTypes('memory_cost', 'int');
         $resolver->setAllowedTypes('time_cost', 'int');
