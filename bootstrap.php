b97cd9d100854669992da5ebb5502a515b20ad9a
Nicholas <47761650+omatamix@users.noreply.github.com>
2021-11-30 05:22:01 -0600

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -28,15 +28,10 @@ declare(strict_types=1);
 
 defined('PASSWORD_PBKDF2') || define('PASSWORD_PBKDF2', 5506);
 
-/**
- * Determine if the hash is pbkdf2.
- *
- * @param string $hash The hash to check.
- *
- * @return bool Returns true if the hash is pbkdf2 and false if not.
- */
-function is_pbkdf2(string $hash): bool
-{
-    $component = substr($hash, 0, 8);
-    return (bool) ($component == '$pbkdf2$');
+if (!function_exists('is_pbkdf2')) {
+    function is_pbkdf2(string $hash): bool
+    {
+        $component = substr($hash, 0, 8);
+        return (bool) ($component == '$pbkdf2$');
+    }
 }
