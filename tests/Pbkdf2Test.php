438e0937d96f39730d538e9d7c6d31eeb418814d
Nicholas <47761650+omatamix@users.noreply.github.com>
2021-12-07 00:41:54 -0600

    Update Pbkdf2Test.php

--- tests/Pbkdf2Test.php
+++ tests/Pbkdf2Test.php
@@ -1,16 +1,36 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
+/**
+ * Omatamix Container
+ *
+ * MIT License
+ * 
+ * Copyright (c) 2022 Nicholas English
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-namespace Omatamix\PasswordLock\Test;
+namespace Omatamix\Password\Test;
 
 use Omatamix\PasswordLock\Pbkdf2;
-use Omatamix\PasswordLock\Exception\PasswordNeedsRehashException;
-use Omatamix\PasswordLock\Exception\PasswordNotVerifiedException;
-
 use PHPUnit\Framework\TestCase;
 
-/**
- * Test pbkdf2 functionality.
- */
 class Pbkdf2Test extends TestCase
 {
     /**
@@ -21,7 +41,7 @@ class Pbkdf2Test extends TestCase
         $pbkdf2 = new Pbkdf2([
             'algo' => 'sha512',
             'salt' => 'Kooser'
-        ], \false);
+        ]);
         $hash = $pbkdf2->compute('Hello World!');
         $this->assertTrue($pbkdf2->verify('Hello World!', $hash));
         $this->assertTrue(!$pbkdf2->verify('Hello Tom!', $hash));
@@ -35,41 +55,9 @@ class Pbkdf2Test extends TestCase
         $pbkdf2 = new Pbkdf2([
             'algo' => 'sha512',
             'salt' => 'Kooser'
-        ], \true);
+        ]);
         $hash = $pbkdf2->compute('Hello World!');
         $this->assertTrue($pbkdf2->verify('Hello World!', $hash));
-    }
-
-    /**
-     * @return void Returns nothing.
-     */
-    public function testPbkdf2HashAgainstExceptionsOnPasswordNeedsRehash(): void
-    {
-        $this->expectException(PasswordNeedsRehashException::class);
-        $pbkdf2 = new Pbkdf2([
-            'algo' => 'sha512',
-            'salt' => 'KooserA'
-        ], \true);
-        $hash = $pbkdf2->compute('Hello World!');
-        $pbkdf2 = new Pbkdf2([
-            'algo' => 'sha512',
-            'salt' => 'KooserB'
-        ], \true);
-        $pbkdf2->needsRehash('Hello World!', $hash);
-    }
-
-    /**
-     * @return void Returns nothing.
-     */
-    public function testPbkdf2HashAgainstExceptionsOnPasswordVerify(): void
-    {
-        $this->expectException(PasswordNotVerifiedException::class);
-        $pbkdf2 = new Pbkdf2([
-            'algo' => 'sha512',
-            'salt' => 'Kooser'
-        ], \true);
-        $hash = $pbkdf2->compute('Hello World!');
-        $res = $pbkdf2->verify('Hello Tom!', $hash);
     }
 
     /**
@@ -78,15 +66,15 @@ class Pbkdf2Test extends TestCase
     public function testPbkdf2HashNeedsRehashAndExceptionOnTrueStatement(): void
     {
         $options = ['algo' => 'sha512', 'salt' => 'KooserA'];
-        $pbkdf2 = new Pbkdf2($options, \false);
+        $pbkdf2 = new Pbkdf2($options);
         $hash = $pbkdf2->compute('Hello World!');
         $this->assertTrue($pbkdf2->verify('Hello World!', $hash));
         $this->assertTrue(!$pbkdf2->verify('Hello Tom!', $hash));
         $options = ['algo' => 'sha512', 'salt' => 'KooserB'];
-        $pbkdf22 = new Pbkdf2($options, \false);
+        $pbkdf22 = new Pbkdf2($options);
         $this->assertTrue($pbkdf22->needsRehash('Hello World!', $hash));
         $options = ['algo' => 'sha512', 'salt' => 'KooserA'];
-        $pbkdf23 = new Pbkdf2($options, \true);
+        $pbkdf23 = new Pbkdf2($options);
         $this->assertTrue(!$pbkdf23->needsRehash('Hello World!', $hash));
     }
 }
