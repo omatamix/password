ae9738c23db2e4946b3ee0fb91958bc306ebdd91
Nicholas <47761650+omatamix@users.noreply.github.com>
2021-12-07 00:50:29 -0600

    Update LockTest.php

--- tests/LockTest.php
+++ tests/LockTest.php
@@ -1,10 +1,35 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
+/**
+ * Omatamix Password
+ *
+ * MIT License
+ * 
+ * Copyright (c) 2022 Nicholas English
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-namespace Omatamix\PasswordLock\Test;
+namespace Omatamix\Password\Test;
 
-use Omatamix\PasswordLock\Bcrypt;
-use Omatamix\PasswordLock\Pbkdf2;
-
+use Omatamix\Password\Bcrypt;
+use Omatamix\Password\Pbkdf2;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -24,8 +49,8 @@ class LockTest extends TestCase
         ]);
         $hash1 = $bcrypt->compute('KooserPHP!');
         $hash2 = $pbkdf2->compute('KooserPHP!');
-        $this->assertTrue(\is_array($bcrypt->getInfo($hash1)));
+        $this->assertTrue(is_array($bcrypt->getInfo($hash1)));
         $info = $pbkdf2->getInfo($hash2);
-        $this->assertTrue(\is_array($info));
+        $this->assertTrue(is_array($info));
     }
 }
