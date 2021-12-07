0a42a8ae6b203d70d0ca1388d00de59ade48db12
Nicholas <47761650+omatamix@users.noreply.github.com>
2021-12-07 00:53:14 -0600

    Update BcryptTest.php

--- tests/BcryptTest.php
+++ tests/BcryptTest.php
@@ -1,14 +1,36 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
+/**
+ * Omatamix Password
+ *
+ * MIT License
+ * 
+ * Copyright (c) 2022 Nicholas English
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-namespace PasswordLock\Test;
+namespace Omatamix\Password\Test;
 
-use Omatamix\PasswordLock\Bcrypt;
-use Omatamix\PasswordLock\Exception\PasswordInvalidException;
-use Omatamix\PasswordLock\Exception\PasswordNeedsRehashException;
-use Omatamix\PasswordLock\Exception\PasswordNotVerifiedException;
-
+use Omatamix\Bcrypt;
+use Omatamix\Exception\PasswordInvalidException;
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\Error\Error;
 
 /**
  * Test bcrypt functionality.
@@ -21,7 +43,7 @@ class BcryptTest extends TestCase
     public function testBcryptHash(): void
     {
         $options = ['cost' => 10];
-        $bcrypt = new Bcrypt($options, \false);
+        $bcrypt = new Bcrypt($options);
         $hash = $bcrypt->compute('Hello World!');
         $this->assertTrue($bcrypt->verify('Hello World!', $hash));
         $this->assertTrue(!$bcrypt->verify('Hello Tom!', $hash));
@@ -33,35 +55,9 @@ class BcryptTest extends TestCase
     public function testBcryptHashAgainstExceptionsOnTrue(): void
     {
         $options = ['cost' => 10];
-        $bcrypt = new Bcrypt($options, \true);
+        $bcrypt = new Bcrypt($options);
         $hash = $bcrypt->compute('Hello World!');
         $this->assertTrue($bcrypt->verify('Hello World!', $hash));
-    }
-
-    /**
-     * @return void Returns nothing.
-     */
-    public function testBcryptHashAgainstExceptionsOnPasswordNeedsRehash(): void
-    {
-        $this->expectException(PasswordNeedsRehashException::class);
-        $options = ['cost' => 10];
-        $bcrypt = new Bcrypt($options, \true);
-        $hash = $bcrypt->compute('Hello World!');
-        $options = ['cost' => 15];
-        $bcrypt = new Bcrypt($options, \true);
-        $bcrypt->needsRehash($hash);
-    }
-
-    /**
-     * @return void Returns nothing.
-     */
-    public function testBcryptHashAgainstExceptionsOnPasswordVerify(): void
-    {
-        $this->expectException(PasswordNotVerifiedException::class);
-        $options = ['cost' => 10];
-        $bcrypt = new Bcrypt($options, \true);
-        $hash = $bcrypt->compute('Hello World!');
-        $res = $bcrypt->verify('Hello Tom!', $hash);
     }
 
     /**
@@ -70,15 +66,15 @@ class BcryptTest extends TestCase
     public function testBcryptHashNeedsRehashAndExceptionOnTrueStatement(): void
     {
         $options = ['cost' => 10];
-        $bcrypt = new Bcrypt($options, \false);
+        $bcrypt = new Bcrypt($options);
         $hash = $bcrypt->compute('Hello World!');
         $this->assertTrue($bcrypt->verify('Hello World!', $hash));
         $this->assertTrue(!$bcrypt->verify('Hello Tom!', $hash));
         $options = ['cost' => 15];
-        $bcrypt2 = new Bcrypt($options, \false);
+        $bcrypt2 = new Bcrypt($options);
         $this->assertTrue($bcrypt2->needsRehash($hash));
         $options = ['cost' => 10];
-        $bcrypt3 = new Bcrypt($options, \true);
+        $bcrypt3 = new Bcrypt($options);
         $this->assertTrue(!$bcrypt3->needsRehash($hash));
     }
 
@@ -89,7 +85,7 @@ class BcryptTest extends TestCase
     {
         $this->expectException(PasswordInvalidException::class);
         $options = ['cost' => 10];
-        $bcrypt = new Bcrypt($options, \true);
+        $bcrypt = new Bcrypt($options);
         $hash = $bcrypt->compute('WWEPt2AEjqqM?DeSV&SXcU=t*^D5Bte#E*R8c3-_kq!bBU$ahjgJFL+Q=2gG?#QqkwzS?qwbs');
     }
 
@@ -98,7 +94,7 @@ class BcryptTest extends TestCase
      */
     public function testBcryptHashInvalidPasswordError(): void
     {
-        $this->expectException(Error::class);
+        $this->expectError();
         $options = ['cost' => 10];
         $bcrypt = new Bcrypt($options, \false);
         $hash = $bcrypt->compute('WWEPt2AEjqqM?DeSV&SXcU=t*^D5Bte#E*R8c3-_kq!bBU$ahjgJFL+Q=2gG?#QqkwzS?qwbs');
